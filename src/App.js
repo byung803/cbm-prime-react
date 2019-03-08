3e3ff202485e310d8cf09acc82ca1b40ab95afef
최병민 <byungminchoi@choebyeongmins-MacBook-Pro.local>
2019-03-08 16:05:52 +0900

    button added but style not work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Button from "./components/button";
 
 class App extends Component {
   render() {
     return (
-      <div>
-      hello
-      </div>
-      );
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/button" component={Button} />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/components/button/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Button from "./Button";
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <Button />
+      </div>
+    );
+  }
+}
